5698d3937ea300a2c9127551ed3ae7065e965169
Christopher Gu <christopher.gu@gmail.com>
2014-08-07 21:42:13 -0500

    Initial Commit

---
+++ Tinderlike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tinderlike
+//
+//  Created by Christopher Gu on 8/7/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tinderlike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tinderlike
+//
+//  Created by Christopher Gu on 8/7/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
